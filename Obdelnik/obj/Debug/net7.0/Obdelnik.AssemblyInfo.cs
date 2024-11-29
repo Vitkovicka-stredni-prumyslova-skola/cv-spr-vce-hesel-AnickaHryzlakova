f3aa2ba33094d9e80de89aa0faa2281cb45d4b62
AnickaHryzlakova <98749@student.vitkovickastredni.cz>
2024-11-29 08:42:45 +0100

    CV - knihovna

--- Obdelnik/obj/Debug/net7.0/Obdelnik.AssemblyInfo.cs
+++ Obdelnik/obj/Debug/net7.0/Obdelnik.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Obdelnik")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df32ae1e7fcf3be2df800ba1510b20b42d49fb82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e89e870767826af14d7af1b2955b4980e1770611")]
 [assembly: System.Reflection.AssemblyProductAttribute("Obdelnik")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Obdelnik")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
